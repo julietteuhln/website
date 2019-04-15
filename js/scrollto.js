3e740dbe6baeafe04e0c684ab73b6c7a0dea3322
jw0nka <julietteuhlen@gmail.com>
2019-04-16 01:35:16 +0200

    hjh

--- js/scrollto.js
+++ js/scrollto.js
@@ -25,52 +25,7 @@ window.onload=function(){
     rowHeight : 400,
     margin: 5,
 
-    }); 
-
-    $(document).ready(function(){
-    $('#article').click(function(){
-        $('.article1').slideDown(2000);
     });
-    $('#retour').click(function(){
-        $('.article1').SlideUp(2000);
-    });
-    });
-
-     $(document).ready(function(){
-    $('#article2').click(function(){
-        $('.article2').show();
-    });
-    $('#retour').click(function(){
-        $('.article2').hide();
-    });
-    });
-
-      $(document).ready(function(){
-    $('#article3').click(function(){
-        $('.article3').show();
-    });
-    $('#retour').click(function(){
-        $('.article3').hide();
-    });
-    });
-
-    $(document).ready(function(){
-    $('#article4').click(function(){
-        $('.article4').show();
-    });
-    $('#retour').click(function(){
-        $('.article4').hide();
-    });
-    });
-     $(document).ready(function(){
-    $('#article5').click(function(){
-        $('.article5').show();
-    });
-    $('#retour').click(function(){
-        $('.article5').hide();
-    });
-    });
-
 
 }
 
